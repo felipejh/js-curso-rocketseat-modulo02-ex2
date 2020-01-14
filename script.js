e8dadda634f6a4b69896bd088c8d2097ef784f2f
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-01-13 21:59:06 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿function adicionarDiv(){
+    var divElement = document.createElement("div");
+    var body = document.body;
+
+    divElement.style.width = 100;
+    divElement.style.height = 100;
+    divElement.style.backgroundColor = "red";
+    divElement.onmouseover = function(){
+        divElement.style.backgroundColor = getRandomColor();
+    };
+
+    body.appendChild(divElement);
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+   }
+
+var btnAdicionarDiv = document.getElementById("btnAdicionarDiv");
+btnAdicionarDiv.onclick = adicionarDiv;
